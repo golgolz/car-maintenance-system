70e5e6133bc288275703b620e41b45e3bf235a03
devpanic <yeon.dev.8@gmail.com>
2024-03-21 23:15:34 +0900

    Revert "feat : 예방 정비 지침 세부 확인 기능 추가"
    This reverts commit d4bc64305b66facd9e2e2c50d115c516832a21f8.

--- car-maintenance-system/src/kr/co/sist/admin/preventi/policy/PreventiPolicyEvent.java
+++ car-maintenance-system/src/kr/co/sist/admin/preventi/policy/PreventiPolicyEvent.java
@@ -1,18 +1,5 @@
 package kr.co.sist.admin.preventi.policy;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+public class PreventiPolicyEvent {
 
-public class PreventiPolicyEvent implements ActionListener {
-    @SuppressWarnings("unused")
-    private PreventiPolicyDialog preventiPolicyDialog;
-
-    public PreventiPolicyEvent(PreventiPolicyDialog preventiPolicyDialog) {
-        this.preventiPolicyDialog = preventiPolicyDialog;
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        preventiPolicyDialog.dispose();
-    }
 }
