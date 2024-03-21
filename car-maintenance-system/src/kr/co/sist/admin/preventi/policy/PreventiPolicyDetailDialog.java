d4bc64305b66facd9e2e2c50d115c516832a21f8
devpanic <yeon.dev.8@gmail.com>
2024-03-21 22:48:30 +0900

    feat : 예방 정비 지침 세부 확인 기능 추가

--- car-maintenance-system/src/kr/co/sist/admin/preventi/policy/PreventiPolicyDetailDialog.java
+++ car-maintenance-system/src/kr/co/sist/admin/preventi/policy/PreventiPolicyDetailDialog.java
@@ -1,5 +1,48 @@
 package kr.co.sist.admin.preventi.policy;
 
-public class PreventiPolicyDetailDialog {
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
 
+@SuppressWarnings("serial")
+public class PreventiPolicyDetailDialog extends JDialog implements ActionListener {
+    public PreventiPolicyDetailDialog(String printData) {
+        super();
+        setLayout(null);
+        String[] printStr = printData.split("/");
+        JLabel jlblTitle = new JLabel(printStr[0]);
+        JLabel jlblContent = new JLabel(printStr[1]);
+        JButton jbtnCancle = new JButton("취소");
+        JButton jbtnOk = new JButton("확인");
+
+        jlblTitle.setFont(new Font("굴림체", Font.BOLD, 25));
+        jlblTitle.setBounds(20, 20, 150, 30);
+        jlblContent.setFont(new Font("굴림체", Font.BOLD, 17));
+        jlblContent.setBounds(20, 60, 400, 30);
+        jbtnCancle.setBounds(20, 110, 200, 40);
+        jbtnOk.setBounds(260, 110, 200, 40);
+        jbtnCancle.setBackground(Color.WHITE);
+        jbtnOk.setBackground(Color.WHITE);
+
+        jbtnCancle.addActionListener(this);
+        jbtnOk.addActionListener(this);
+
+        add(jlblTitle);
+        add(jlblContent);
+        add(jbtnCancle);
+        add(jbtnOk);
+
+        setSize(500, 200);
+        setVisible(true);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        dispose();
+    }
 }
--- car-maintenance-system/src/kr/co/sist/admin/preventi/policy/PreventiPolicyDialog.java
+++ car-maintenance-system/src/kr/co/sist/admin/preventi/policy/PreventiPolicyDialog.java
@@ -1,64 +1,176 @@
 package kr.co.sist.admin.preventi.policy;
 
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.SQLException;
 import java.util.List;
+import javax.swing.DefaultCellEditor;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
 
 @SuppressWarnings("serial")
 public class PreventiPolicyDialog extends JDialog {
+    private DefaultTableModel dtmPolicies;
+    private JTable jtblPolicies;
+    private JScrollPane jsp;
+
     public PreventiPolicyDialog() {
         super();
         setLayout(null);
         JLabel jlblTitle = new JLabel("예방 정비 지침");
         JLabel jlblContent = new JLabel("예방 정비 지침에 대한 안내입니다.");
+        JButton jbtnCancle = new JButton("취소");
+        JButton jbtnOk = new JButton("확인");
 
-        String[] policyHeader = {"부품코드", "부품명", "주행거리 기준", "제조일 기준", "상세보기"};
-        String[][] dumpData = null;
+        String[] policyHeader = {"부품코드", "부품명", "주행거리 기준", "제조일 기준", "상세보기", "상세내용"};
+        Object[][] policyData = null;
 
         try {
-            dumpData = createPolicyData();
+            policyData = createPolicyData();
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
-        DefaultTableModel dtmPolicies = new DefaultTableModel(dumpData, policyHeader);
-        JTable jtblPolicies = new JTable(dtmPolicies);
-        JScrollPane jsp = new JScrollPane(jtblPolicies);
+        dtmPolicies = new DefaultTableModel(policyData, policyHeader);
+        jtblPolicies = new JTable(dtmPolicies);
+        jsp = new JScrollPane(jtblPolicies);
+
+        jbtnCancle.setBounds(20, 310, 200, 40);
+        jbtnOk.setBounds(260, 310, 200, 40);
+        jbtnCancle.setBackground(Color.WHITE);
+        jbtnOk.setBackground(Color.WHITE);
+
+        jtblPolicies.getColumnModel().getColumn(5).setMaxWidth(0);
+        jtblPolicies.getColumnModel().getColumn(5).setMinWidth(0);
+        jtblPolicies.getColumnModel().getColumn(5).setWidth(0);
+        jtblPolicies.getColumnModel().getColumn(5).setPreferredWidth(0);
+        jtblPolicies.getColumnModel().getColumn(3).setWidth(110);
+        jtblPolicies.getColumnModel().getColumn(4).setCellRenderer(new ButtonRenderer());
+        jtblPolicies.getColumnModel().getColumn(4).setCellEditor(new ButtonEditor(new JCheckBox()));
+
+        int jtbWidth = 0;
+        for (int i = 0; i < 6; i++) {
+            jtbWidth += jtblPolicies.getColumnModel().getColumn(i).getWidth();
+        }
+        jsp.setBounds(20, 110, jtbWidth, 120);
 
         jlblTitle.setFont(new Font("굴림체", Font.BOLD, 25));
         jlblTitle.setBounds(20, 20, 260, 40);
         jlblContent.setFont(new Font("굴림체", Font.PLAIN, 15));
         jlblContent.setBounds(25, 60, 260, 40);
-        jsp.setBounds(20, 110, 300, 120);
+
+        jbtnCancle.addActionListener(new PreventiPolicyEvent(this));
+        jbtnOk.addActionListener(new PreventiPolicyEvent(this));
 
         add(jlblTitle);
         add(jlblContent);
         add(jsp);
+        add(jbtnCancle);
+        add(jbtnOk);
 
         setSize(840, 480);
         setVisible(true);
         setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
     }
 
-    public String[][] createPolicyData() throws SQLException {
+    public DefaultTableModel getDtmPolicies() {
+        return dtmPolicies;
+    }
+
+    public JTable getJtblPolicies() {
+        return jtblPolicies;
+    }
+
+    public JScrollPane getJsp() {
+        return jsp;
+    }
+
+    static class ButtonRenderer extends JButton implements TableCellRenderer {
+        public ButtonRenderer() {
+            setOpaque(true);
+        }
+
+        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
+                int row, int column) {
+            setText("보기");
+            return this;
+        }
+    }
+
+    static class ButtonEditor extends DefaultCellEditor {
+        private JButton button;
+        private String rowData;
+
+        public ButtonEditor(JCheckBox checkBox) {
+            super(checkBox);
+            button = new JButton();
+            button.setOpaque(true);
+            button.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    fireEditingStopped();
+                    // JOptionPane.showMessageDialog(button, rowData);
+                    new PreventiPolicyDetailDialog(rowData);
+                }
+            });
+        }
+
+        public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row,
+                int column) {
+            if (isSelected) {
+                button.setForeground(table.getSelectionForeground());
+                button.setBackground(table.getSelectionBackground());
+            } else {
+                button.setForeground(table.getForeground());
+                button.setBackground(table.getBackground());
+            }
+
+            rowData = getRowData(table, row);
+            button.setText("보기");
+            return button;
+        }
+
+        private String getRowData(JTable table, int row) {
+            StringBuilder rowData = new StringBuilder();
+            rowData.append(table.getModel().getValueAt(row, 1)).append("/");
+            rowData.append(table.getModel().getValueAt(row, 5));
+            return rowData.toString();
+        }
+
+        public Object getCellEditorValue() {
+            return "";
+        }
+    }
+
+    public Object[][] createPolicyData() throws SQLException {
         List<PreventiPolicyVO> tempPolicies = PreventiPolicyDAO.getInstance().selectAllPolicies();
-        String[][] policies = new String[tempPolicies.size()][5];
+        Object[][] policies = new String[tempPolicies.size()][6];
 
         PreventiPolicyVO tempVO = null;
+        int tempInt = 0;
 
         for (int i = 0; i < policies.length; i++) {
             tempVO = tempPolicies.get(i);
             policies[i][0] = tempVO.getPart();
-            // policies[i][1] = tempVO.getDistancePeriod();
-            policies[i][1] = "-";
-            policies[i][2] = Integer.toString(tempVO.getDistancePeriod());
-            policies[i][3] = Integer.toString(tempVO.getManufacturePeriod());
-            policies[i][4] = tempVO.getContent();
+            policies[i][1] = "엔진오일";
+            policies[i][2] = "매 " + Integer.toString(tempVO.getDistancePeriod()) + "km";
+            policies[i][4] = "보기";
+            policies[i][5] = tempVO.getContent();
+
+            tempInt = tempVO.getManufacturePeriod();
+            if (tempInt == 0) {
+                policies[i][3] = "없음";
+            } else {
+                policies[i][3] = Integer.toString(tempInt) + "개월";
+            }
         }
         return policies;
     }
