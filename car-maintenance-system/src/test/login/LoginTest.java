32b71eaf81c56d30216d390fc2b73a0ffd6c02bc
ddinghwa <cpfl688@naver.com>
2024-03-21 10:58:42 +0900

    Test: 로그인 테스트를 위한 LoginTest 파일

---
+++ car-maintenance-system/src/test/login/LoginTest.java
@@ -0,0 +1,13 @@
+package test.login;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class LoginTest {
+
+  @Test
+  void test() {
+    fail("Not yet implemented");
+  }
+
+}
