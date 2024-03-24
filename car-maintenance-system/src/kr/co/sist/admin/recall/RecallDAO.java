8694c4f4e41a9e1c994791e3d6ae2736f00b0339
devpanic <yeon.dev.8@gmail.com>
2024-03-24 22:57:23 +0900

    feat : 리콜 상세 정보 조회 다이얼로그 추가, 사용자 예방 정비 대상 차량 조회 프레임 추가

--- car-maintenance-system/src/kr/co/sist/admin/recall/RecallDAO.java
+++ car-maintenance-system/src/kr/co/sist/admin/recall/RecallDAO.java
@@ -1,5 +1,73 @@
 package kr.co.sist.admin.recall;
 
-public class RecallDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import kr.co.sist.dao.DBConnection;
 
+public class RecallDAO {
+    static private RecallDAO recallDAO;
+    private Connection conn = null;
+    private PreparedStatement pstmt = null;
+    private ResultSet resultSet = null;
+
+    private RecallDAO() {}
+
+    static public RecallDAO getInstance() {
+        if (recallDAO == null) {
+            recallDAO = new RecallDAO();
+        }
+
+        return recallDAO;
+    }
+
+    public RecallInfoVO selectOneRecallInfo(String carId) {
+        RecallInfoVO recallInfo = null;
+        DBConnection dbConn = DBConnection.getInstance();
+
+        try {
+            StringBuilder selectQuery = new StringBuilder();
+            selectQuery.append(" select part_info.part_name, cmir.car_model, cmir.image, cmir.content ")
+                    .append(" from car_model_info_recall cmir ")
+                    .append(" join part_info on part_info.part_code = cmir.part_code ")
+                    .append(" join resisted_car on resisted_car.car_model = cmir.car_model ")
+                    .append(" where resisted_car.car_id = ? ");
+            conn = dbConn.getConnection();
+            pstmt = conn.prepareStatement(selectQuery.toString());
+            pstmt.setString(1, carId);
+
+            resultSet = pstmt.executeQuery();
+
+            while (resultSet.next()) {
+                recallInfo = new RecallInfoVO(resultSet.getString("part_name"), resultSet.getString("car_model"),
+                        resultSet.getString("image"));
+                // System.out.println(resultSet.getString("content"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                dbConn.dbClose(conn, pstmt, resultSet);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return recallInfo;
+    }
+
+    public List<RecallTargetVO> selectAllRecallTarget() {
+        List<RecallTargetVO> recallTargets = new ArrayList<RecallTargetVO>();
+
+        return recallTargets;
+    }
+
+    public List<RecallTargetVO> selectRecallTargets(String carId, String ownerId) {
+        List<RecallTargetVO> recallTargets = new ArrayList<RecallTargetVO>();
+
+        return recallTargets;
+    }
 }
--- car-maintenance-system/src/kr/co/sist/user/recall/UserRecallDetailDialogEvent.java
+++ car-maintenance-system/src/kr/co/sist/user/recall/UserRecallDetailDialogEvent.java
@@ -1,5 +1,25 @@
 package kr.co.sist.user.recall;
 
-public class UserRecallDetailDialogEvent {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+public class UserRecallDetailDialogEvent implements ActionListener {
+    private UserRecallDetailDialogView userRecallDetailDialogView;
+
+    public UserRecallDetailDialogEvent(UserRecallDetailDialogView userRecallDetailDialogView) {
+        this.userRecallDetailDialogView = userRecallDetailDialogView;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        switch (e.getActionCommand()) {
+            case "예약하기":
+                System.out.println("예약 버튼");
+                // [TODO] 예약 다이얼로그 객체화 추가
+                break;
+            case "확인":
+                userRecallDetailDialogView.dispose();
+                break;
+        }
+    }
 }
