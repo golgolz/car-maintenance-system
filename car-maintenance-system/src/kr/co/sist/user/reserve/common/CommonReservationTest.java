8a1ae59ad77ad77e94860e6ed57dff4643348d81
jmh <tellis0207@gmail.com>
2024-03-25 17:54:21 +0900

    feat : 일반 정비 예약에 대한 view,event,dao,vo 구현

---
+++ car-maintenance-system/src/kr/co/sist/user/reserve/common/CommonReservationTest.java
@@ -0,0 +1,36 @@
+package kr.co.sist.user.reserve.common;
+
+import org.junit.jupiter.api.Test;
+import kr.co.sist.user.reserve.dialog.ReservationDialogView;
+
+public class CommonReservationTest {
+  @Test
+  void testCommon() {
+    CommonReservationView crv = new CommonReservationView("일반 정비 예약", ReservationDialogView.COMMON);
+    crv.setVisible(true);
+
+    while (crv.isVisible()) {
+    } // end while
+  }// testCommon
+
+  @Test
+  void testPreventi() {
+    CommonReservationView crv = new CommonReservationView("예방 정비 예약", ReservationDialogView.PREVENTI);
+    crv.setVisible(true);
+
+    while (crv.isVisible()) {
+    } // end while
+  }// testCommon
+
+  @Test
+  void testRecall() {
+    CommonReservationView crv = new CommonReservationView("리콜 예약", ReservationDialogView.RECALL);
+    crv.setVisible(true);
+
+    while (crv.isVisible()) {
+    } // end while
+  }// testCommon
+
+}
+
+
