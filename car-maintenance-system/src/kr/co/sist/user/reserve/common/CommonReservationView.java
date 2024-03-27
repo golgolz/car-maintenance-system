95936ccd844a42d26fb8828700756199d7af8c57
jmh <tellis0207@gmail.com>
2024-03-27 18:07:59 +0900

    feat: VO에 매개변수 tel 추가, Event 수정, DAO 수정(tel 추가 조회)

--- car-maintenance-system/src/kr/co/sist/user/reserve/common/CommonReservationView.java
+++ car-maintenance-system/src/kr/co/sist/user/reserve/common/CommonReservationView.java
@@ -4,72 +4,76 @@ import java.awt.Color;
 import java.awt.Font;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
+import kr.co.sist.FontSingleton;
 import kr.co.sist.user.reserve.dialog.ReservationDialogView;
 
 
 public class CommonReservationView extends JFrame {
 
-  private JLabel jlReservationManagement;
+    private JLabel jlReservationManagement;
+    private String parts;
+    private JTextField jtfDate;
 
 
-  public CommonReservationView(String label, int viewNum) {
-    super(label);
+    public CommonReservationView(String label, int viewNum, String parts) {
+        super(label);
+        this.parts = parts;
+        // 프레임의 배경색 설정
+        this.getContentPane().setBackground(Color.decode("#002347"));
 
-    try {
-      UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
-    } catch (ClassNotFoundException | InstantiationException | IllegalAccessException
-        | UnsupportedLookAndFeelException e) {
-      e.printStackTrace();
-    } // end catch
+        try {
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+                | UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        } // end catch
 
-    SwingUtilities.updateComponentTreeUI(this);
-
-
-    jlReservationManagement = new JLabel(label);
-    // jlReservationManagement.setFont(FontSingleton.getInstance().bonGodic.deriveFont(Font.BOLD, 27f));
-    ReservationDialogView rdv = new ReservationDialogView(viewNum, this); // 1은 일반 정비 예약
+        SwingUtilities.updateComponentTreeUI(this);
 
 
-    Font font = new Font("맑은 고딕", Font.BOLD, 24);
-    jlReservationManagement.setFont(font);
-    jlReservationManagement.setForeground(Color.WHITE);
-
-    setLayout(null);
-    jlReservationManagement.setBounds(320, 50, 400, 60);
-    rdv.setBounds(100, 100, 600, 320);
-    rdv.setBorder(new TitledBorder(new LineBorder(Color.BLACK, 1)));
+        jlReservationManagement = new JLabel(label);
+        jlReservationManagement.setFont(FontSingleton.getInstance().bonGodic.deriveFont(Font.BOLD, 27f));
+        ReservationDialogView rdv = new ReservationDialogView(viewNum, this); // 1은 일반 정비 예약
 
 
-    add(jlReservationManagement); // 라벨 배치
-    add(rdv); // rdv를 CommonReservationView에 추가
+        Font font = FontSingleton.getInstance().bonGodic.deriveFont(16f);
+        jlReservationManagement.setFont(font);
+        jlReservationManagement.setForeground(Color.WHITE);
 
-    // 프레임의 배경색 설정
-    Color backgroundColor = new Color(14, 50, 74);
-    getContentPane().setBackground(backgroundColor);
+        setLayout(null);
+        jlReservationManagement.setBounds(320, 50, 400, 60);
+        rdv.setBounds(100, 100, 600, 320);
+        rdv.setBorder(new TitledBorder(new LineBorder(Color.BLACK, 1)));
+
+        add(jlReservationManagement); // 라벨 배치
+        add(rdv); // rdv를 CommonReservationView에 추가
+
+        // 이벤트 추가
+        CommonReservationEvent commonReservationEvent = new CommonReservationEvent(this);
+        addWindowListener(commonReservationEvent);
+
+        setBounds(500, 300, 840, 480);
+        setResizable(false);
+        setVisible(true);
+    }// CommonReservationView
 
 
-    // 이벤트 추가
-    CommonReservationEvent commonReservationEvent = new CommonReservationEvent(this);
-    addWindowListener(commonReservationEvent);
+    public String getParts() {
+        return parts;
+    }
 
-    setBounds(500, 300, 840, 480);
-    setResizable(false);
-    setVisible(true);
-  }// CommonReservationView
-
-
-
-  public static void main(String[] args) {
-    new CommonReservationView("일반 정비 예약", ReservationDialogView.COMMON);
+    // public static void main(String[] args) {
+    // new CommonReservationView("일반 정비 예약", ReservationDialogView.COMMON);
     // new CommonReservationView("예방 정비 예약",ReservationDialogView.PREVENTI);
     // new CommonReservationView("리콜 예약",ReservationDialogView.RECALL);
-
-  }
+    //
+    // }
 
 
 }
--- car-maintenance-system/src/kr/co/sist/user/reserve/dialog/ReservationDialogView.java
+++ car-maintenance-system/src/kr/co/sist/user/reserve/dialog/ReservationDialogView.java
@@ -10,244 +10,248 @@ import javax.swing.JRadioButton;
 import javax.swing.JTextArea;
 import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
+import kr.co.sist.FontSingleton;
 import kr.co.sist.user.reserve.common.CommonReservationView;
 
 @SuppressWarnings("serial")
 public class ReservationDialogView extends JPanel {
 
-  public static final int COMMON = 1; // 일반
-  public static final int PREVENTI = 2; // 예방
-  public static final int RECALL = 3; // 리콜
+    public static final int COMMON = 1; // 일반
+    public static final int PREVENTI = 2; // 예방
+    public static final int RECALL = 3; // 리콜
 
-  private int viewNum;
+    private int viewNum;
 
-  private JLabel jlReservationDate;
-  private JLabel jlAM;
-  private JLabel jlPM;
-  private JLabel jlReservationReason;
-  private JTextArea jtaReservationReason;
-  private JRadioButton jrbAM;
-  private JRadioButton jrbPM;
-  private JButton jbtnConfirm;
-  private JButton jbtnCancel;
+    private JLabel jlReservationDate;
+    private JLabel jlAM;
+    private JLabel jlPM;
+    private JLabel jlReservationReason;
+    private JTextArea jtaReservationReason;
+    private JRadioButton jrbAM;
+    private JRadioButton jrbPM;
+    private JButton jbtnConfirm;
+    private JButton jbtnCancel;
 
-  private CommonReservationView crv;
+    private CommonReservationView crv;
 
-  public ReservationDialogView(int viewNum, CommonReservationView crv) {
+    public ReservationDialogView(int viewNum, CommonReservationView crv) {
 
-    this.crv = crv;
-    this.viewNum = viewNum;
-    ReservationDialogEvent rde = new ReservationDialogEvent(this);
+        this.crv = crv;
+        this.viewNum = viewNum;
+        ReservationDialogEvent rde = new ReservationDialogEvent(this);
 
-    if (viewNum == COMMON) { // 일반 정비 예약
-      addCommonView();
-    } // end if
-    if (viewNum == PREVENTI) { // 예방 정비 예약
-      addPreventiView();
-    } // end if
-    if (viewNum == RECALL) { // 리콜 예약
-      addRecallView();
-    } // end if
+        if (viewNum == COMMON) { // 일반 정비 예약
+            addCommonView();
+        } // end if
+        if (viewNum == PREVENTI) { // 예방 정비 예약
+            addPreventiView();
+        } // end if
+        if (viewNum == RECALL) { // 리콜 예약
+            addRecallView();
+        } // end if
 
-    // 이벤트 추가
-    jbtnConfirm.addActionListener(rde);
-    jbtnCancel.addActionListener(rde);
-    jrbAM.addActionListener(rde);
-    jrbPM.addActionListener(rde);
+        // 이벤트 추가
+        jbtnConfirm.addActionListener(rde);
+        jbtnCancel.addActionListener(rde);
+        jrbAM.addActionListener(rde);
+        jrbPM.addActionListener(rde);
 
-    setVisible(true);
+        setVisible(true);
 
-  }
+    }
 
-  private void addCommonView() {
-    Font font = new Font("맑은 고딕", Font.BOLD, 18);
-    // 캘린더
-    jlReservationDate = new JLabel();
+    private void addCommonView() {
+        Font font = new Font("맑은 고딕", Font.BOLD, 18);
+        // 캘린더
+        jlReservationDate = new JLabel();
 
-    jlReservationDate.setBorder(new TitledBorder(new LineBorder(Color.BLACK, 1)));
+        jlReservationDate.setBorder(new TitledBorder(new LineBorder(Color.BLACK, 1)));
 
-    // 라디오 버튼
-    jrbAM = new JRadioButton("오전");
-    jrbPM = new JRadioButton("오후");
-    ButtonGroup bg = new ButtonGroup();
-    bg.add(jrbAM);
-    bg.add(jrbPM);
+        // 라디오 버튼
+        jrbAM = new JRadioButton("오전");
+        jrbPM = new JRadioButton("오후");
+        ButtonGroup bg = new ButtonGroup();
+        bg.add(jrbAM);
+        bg.add(jrbPM);
 
-    jlAM = new JLabel("오전");
-    jlPM = new JLabel("오후");
-    jlAM.setFont(new Font("맑은 고딕", Font.BOLD, 13));
-    jlPM.setFont(new Font("맑은 고딕", Font.BOLD, 13));
+        jlAM = new JLabel("오전");
+        jlPM = new JLabel("오후");
+        jlAM.setFont(FontSingleton.getInstance().bonGodic.deriveFont(16f));
+        jlPM.setFont(FontSingleton.getInstance().bonGodic.deriveFont(16f));
 
-    // 예약 사유
-    jlReservationReason = new JLabel("사후관리 요청 내용");
-    jlReservationReason.setFont(font);
-    jtaReservationReason = new JTextArea("요청 내용을 입력해주세요");
+        // 예약 사유
+        jlReservationReason = new JLabel("사후관리 요청 내용");
+        jlReservationReason.setFont(font);
+        jtaReservationReason = new JTextArea("요청 내용을 입력해주세요");
 
-    // 하단 버튼
-    jbtnConfirm = new JButton("예약신청");
-    jbtnCancel = new JButton("취소");
+        // 하단 버튼
+        jbtnConfirm = new JButton("예약신청");
+        jbtnCancel = new JButton("취소");
 
-    setLayout(null);
+        setLayout(null);
 
-    ReservationCalendarDialogView rcdv = new ReservationCalendarDialogView();
-    rcdv.setSize(200, 200);
-    rcdv.setBorder(new TitledBorder(new LineBorder(Color.BLACK)));
-    jlReservationDate.add(rcdv);
+        ReservationCalendarDialogView rcdv = new ReservationCalendarDialogView();
+        rcdv.setSize(200, 200);
+        rcdv.setBorder(new TitledBorder(new LineBorder(Color.BLACK)));
+        jlReservationDate.add(rcdv);
 
-    // 컴포넌트 배치
-    jlReservationDate.setBounds(10, 20, 230, 220);
-    rcdv.setBounds(0, 0, 230, 220);
-    jrbAM.setBounds(290, 30, 20, 20);
-    jlAM.setBounds(320, 30, 40, 20);
-    jrbPM.setBounds(360, 30, 20, 20);
-    jlPM.setBounds(390, 30, 40, 20);
-    jlReservationReason.setBounds(270, 70, 180, 40);
-    jtaReservationReason.setBounds(270, 120, 310, 120);
-    jbtnConfirm.setBounds(200, 270, 90, 30);
-    jbtnCancel.setBounds(300, 270, 90, 30);
+        // 컴포넌트 배치
+        jlReservationDate.setBounds(10, 20, 230, 220);
+        rcdv.setBounds(0, 0, 230, 220);
+        jrbAM.setBounds(290, 30, 20, 20);
+        jlAM.setBounds(320, 30, 40, 20);
+        jrbPM.setBounds(360, 30, 20, 20);
+        jlPM.setBounds(390, 30, 40, 20);
+        jlReservationReason.setBounds(270, 70, 180, 40);
+        jtaReservationReason.setBounds(270, 120, 310, 120);
+        jbtnConfirm.setBounds(200, 270, 90, 30);
+        jbtnCancel.setBounds(300, 270, 90, 30);
 
-    add(jlReservationDate);
-    add(jrbAM);
-    add(jlAM);
-    add(jrbPM);
-    add(jlPM);
-    add(jlReservationReason);
-    add(jtaReservationReason);
-    add(jbtnConfirm);
-    add(jbtnCancel);
-  }// addCommonView
-
-
-  private void addPreventiView() {
-    Font font = new Font("맑은 고딕", Font.BOLD, 18);
-    // 캘린더 라벨
-    jlReservationDate = new JLabel();
-
-    jlReservationDate.setBorder(new TitledBorder(new LineBorder(Color.BLACK, 1)));
-
-    // 라디오 버튼
-    jrbAM = new JRadioButton();
-    jrbPM = new JRadioButton();
-    ButtonGroup bg = new ButtonGroup();
-    bg.add(jrbAM);
-    bg.add(jrbPM);
-
-    jlAM = new JLabel("오전");
-    jlPM = new JLabel("오후");
-    jlAM.setFont(new Font("맑은 고딕", Font.BOLD, 13));
-    jlPM.setFont(new Font("맑은 고딕", Font.BOLD, 13));
-
-    // 하단 버튼
-    jbtnConfirm = new JButton("예약확정");
-    jbtnCancel = new JButton("취소");
-
-    setLayout(null);
-
-    ReservationCalendarDialogView rcdv = new ReservationCalendarDialogView();
-    rcdv.setSize(200, 200);
-    rcdv.setBorder(new TitledBorder(new LineBorder(Color.BLACK)));
-    jlReservationDate.add(rcdv);
-
-    // 컴포넌트 배치
-    jlReservationDate.setBounds(10, 20, 230, 220);
-    rcdv.setBounds(0, 0, 230, 220);
-    jrbAM.setBounds(290, 120, 20, 20);
-    jlAM.setBounds(320, 120, 40, 20);
-    jrbPM.setBounds(360, 120, 20, 20);
-    jlPM.setBounds(390, 120, 40, 20);
-    jbtnConfirm.setBounds(200, 270, 90, 30);
-    jbtnCancel.setBounds(300, 270, 90, 30);
-
-    add(jlReservationDate);
-    add(jrbAM);
-    add(jlAM);
-    add(jrbPM);
-    add(jlPM);
-    add(jbtnConfirm);
-    add(jbtnCancel);
-  }// addPreventiView
+        add(jlReservationDate);
+        add(jrbAM);
+        add(jlAM);
+        add(jrbPM);
+        add(jlPM);
+        add(jlReservationReason);
+        add(jtaReservationReason);
+        add(jbtnConfirm);
+        add(jbtnCancel);
+    }// addCommonView
 
 
-  private void addRecallView() {
-    Font font = new Font("맑은 고딕", Font.BOLD, 18);
-    // 캘린더 라벨
-    jlReservationDate = new JLabel();
+    private void addPreventiView() {
+        // 캘린더 라벨
+        jlReservationDate = new JLabel();
 
-    jlReservationDate.setBorder(new TitledBorder(new LineBorder(Color.BLACK, 1)));
+        jlReservationDate.setBorder(new TitledBorder(new LineBorder(Color.BLACK, 1)));
 
-    // 라디오 버튼
-    jrbAM = new JRadioButton();
-    jrbPM = new JRadioButton();
-    ButtonGroup bg = new ButtonGroup();
-    bg.add(jrbAM);
-    bg.add(jrbPM);
+        // 라디오 버튼
+        jrbAM = new JRadioButton("오전");
+        jrbPM = new JRadioButton("오후");
+        ButtonGroup bg = new ButtonGroup();
+        bg.add(jrbAM);
+        bg.add(jrbPM);
 
-    jlAM = new JLabel("오전");
-    jlPM = new JLabel("오후");
-    jlAM.setFont(new Font("맑은 고딕", Font.BOLD, 13));
-    jlPM.setFont(new Font("맑은 고딕", Font.BOLD, 13));
+        jlAM = new JLabel("오전");
+        jlPM = new JLabel("오후");
+        jlAM.setFont(FontSingleton.getInstance().bonGodic.deriveFont(16f));
+        jlPM.setFont(FontSingleton.getInstance().bonGodic.deriveFont(16f));
 
-    // 하단 버튼
-    jbtnConfirm = new JButton("예약신청");
-    jbtnCancel = new JButton("취소");
+        // 하단 버튼
+        jbtnConfirm = new JButton("예약확정");
+        jbtnCancel = new JButton("취소");
 
-    setLayout(null);
+        setLayout(null);
 
-    ReservationCalendarDialogView rcdv = new ReservationCalendarDialogView();
-    rcdv.setSize(200, 200);
-    rcdv.setBorder(new TitledBorder(new LineBorder(Color.BLACK)));
-    jlReservationDate.add(rcdv);
+        ReservationCalendarDialogView rcdv = new ReservationCalendarDialogView();
+        rcdv.setSize(200, 200);
+        rcdv.setBorder(new TitledBorder(new LineBorder(Color.BLACK)));
+        jlReservationDate.add(rcdv);
 
-    // 컴포넌트 배치
-    jlReservationDate.setBounds(10, 20, 230, 220);
-    rcdv.setBounds(0, 0, 230, 220);
-    jrbAM.setBounds(290, 120, 20, 20);
-    jlAM.setBounds(320, 120, 40, 20);
-    jrbPM.setBounds(360, 120, 20, 20);
-    jlPM.setBounds(390, 120, 40, 20);
-    jbtnConfirm.setBounds(200, 270, 90, 30);
-    jbtnCancel.setBounds(300, 270, 90, 30);
+        // 컴포넌트 배치
+        jlReservationDate.setBounds(10, 20, 230, 220);
+        rcdv.setBounds(0, 0, 230, 220);
+        jrbAM.setBounds(290, 120, 20, 20);
+        jlAM.setBounds(320, 120, 40, 20);
+        jrbPM.setBounds(360, 120, 20, 20);
+        jlPM.setBounds(390, 120, 40, 20);
+        jbtnConfirm.setBounds(200, 270, 90, 30);
+        jbtnCancel.setBounds(300, 270, 90, 30);
 
-    add(jlReservationDate);
-    add(jrbAM);
-    add(jlAM);
-    add(jrbPM);
-    add(jlPM);
-    add(jbtnConfirm);
-    add(jbtnCancel);
-  }// addRecallView
+        add(jlReservationDate);
+        add(jrbAM);
+        add(jlAM);
+        add(jrbPM);
+        add(jlPM);
+        add(jbtnConfirm);
+        add(jbtnCancel);
+    }// addPreventiView
 
 
-  public JLabel getJlReservationDate() {
-    return jlReservationDate;
-  }
+    private void addRecallView() {
+        // 캘린더 라벨
+        jlReservationDate = new JLabel();
 
-  public JLabel getJlReservationReason() {
-    return jlReservationReason;
-  }
+        jlReservationDate.setBorder(new TitledBorder(new LineBorder(Color.BLACK, 1)));
 
-  public JTextArea getJtaReservationReason() {
-    return jtaReservationReason;
-  }
+        // 라디오 버튼
+        jrbAM = new JRadioButton("오전");
+        jrbPM = new JRadioButton("오후");
+        ButtonGroup bg = new ButtonGroup();
+        bg.add(jrbAM);
+        bg.add(jrbPM);
 
-  public JRadioButton getJrbAM() {
-    return jrbAM;
-  }
+        jlAM = new JLabel("오전");
+        jlPM = new JLabel("오후");
+        jlAM.setFont(FontSingleton.getInstance().bonGodic.deriveFont(16f));
+        jlPM.setFont(FontSingleton.getInstance().bonGodic.deriveFont(16f));
 
-  public JRadioButton getJrbPM() {
-    return jrbPM;
-  }
+        // 하단 버튼
+        jbtnConfirm = new JButton("예약신청");
+        jbtnCancel = new JButton("취소");
 
-  public JButton getJbtnConfirm() {
-    return jbtnConfirm;
-  }
+        setLayout(null);
 
-  public JButton getJbtnCancel() {
-    return jbtnCancel;
-  }
+        ReservationCalendarDialogView rcdv = new ReservationCalendarDialogView();
+        rcdv.setSize(200, 200);
+        rcdv.setBorder(new TitledBorder(new LineBorder(Color.BLACK)));
+        jlReservationDate.add(rcdv);
 
-  public int getViewNum() {
-    return viewNum;
-  }
+        // 컴포넌트 배치
+        jlReservationDate.setBounds(10, 20, 230, 220);
+        rcdv.setBounds(0, 0, 230, 220);
+        jrbAM.setBounds(290, 120, 20, 20);
+        jlAM.setBounds(320, 120, 40, 20);
+        jrbPM.setBounds(360, 120, 20, 20);
+        jlPM.setBounds(390, 120, 40, 20);
+        jbtnConfirm.setBounds(200, 270, 90, 30);
+        jbtnCancel.setBounds(300, 270, 90, 30);
+
+        add(jlReservationDate);
+        add(jrbAM);
+        add(jlAM);
+        add(jrbPM);
+        add(jlPM);
+        add(jbtnConfirm);
+        add(jbtnCancel);
+    }// addRecallView
+
+
+    public JLabel getJlReservationDate() {
+        return jlReservationDate;
+    }
+
+    public JLabel getJlReservationReason() {
+        return jlReservationReason;
+    }
+
+    public JTextArea getJtaReservationReason() {
+        return jtaReservationReason;
+    }
+
+    public JRadioButton getJrbAM() {
+        return jrbAM;
+    }
+
+    public JRadioButton getJrbPM() {
+        return jrbPM;
+    }
+
+    public JButton getJbtnConfirm() {
+        return jbtnConfirm;
+    }
+
+    public JButton getJbtnCancel() {
+        return jbtnCancel;
+    }
+
+    public int getViewNum() {
+        return viewNum;
+    }
+
+    public CommonReservationView getCrv() {
+        return crv;
+    }
+
 
 }
