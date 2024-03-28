1b544823e9d0f616f385b345f9bb5bc1baa0e7fc
devpanic <yeon.dev.8@gmail.com>
2024-03-28 18:47:03 +0900

    design, fix : 뷰 구성 일관적으로 변경, 사용자 예방 정비 관리 버그 수정
    fix - 사용자 예방 정비 대상이 선택되지 않은 경우 처리

--- car-maintenance-system/src/kr/co/sist/admin/manage/reservation/common/CommonReservationView.java
+++ car-maintenance-system/src/kr/co/sist/admin/manage/reservation/common/CommonReservationView.java
@@ -1,5 +1,7 @@
 package kr.co.sist.admin.manage.reservation.common;
 
+import java.awt.Color;
+import java.awt.Font;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -7,6 +9,7 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
+import kr.co.sist.FontSingleton;
 
 @SuppressWarnings("serial")
 public class CommonReservationView extends JFrame {
@@ -17,45 +20,51 @@ public class CommonReservationView extends JFrame {
 
     public CommonReservationView() {
         super("차량 정비 관리 시스템");
+        this.getContentPane().setBackground(Color.decode("#002347"));
+        setLayout(null);
 
         jlbCommonReservationTitle = new JLabel("일반 정비 신청 차량");
         jlbCarIdTitle = new JLabel("차량번호");
         jlbOwnerName = new JLabel("고객명");
-
         jbtnSearch = new JButton("검색");
-
         jtfCarId = new JTextField();
         jtfOwnerName = new JTextField();
 
-        String[] HeaderInfo = {"차량번호", "고객명", "ID", "모델", "주행거리", "정비상태", "차량 제조일", "예약일", "에약사유"};
+        String[] HeaderInfo = {"차량번호", "고객명", "ID", "모델", "주행거리", "정비상태", "차량 제조일", "예약일", "예약사유"};
         String[][] dumpData = null;
         DefaultTableModel commonReservation = new DefaultTableModel(dumpData, HeaderInfo);
         JTable commonReservationTable = new JTable(commonReservation);
-        JScrollPane jsp = new JScrollPane(commonReservationTable);
+        commonReservationTable.getTableHeader().setFont(FontSingleton.getInstance().bonGodic.deriveFont(12f));
+        commonReservationTable.setFont(FontSingleton.getInstance().bonGodic.deriveFont(11f));
 
+        JScrollPane jsp = new JScrollPane(commonReservationTable);
         commonReservationTable = new JTable(commonReservation);
 
-        setLayout(null);
+        jlbCommonReservationTitle.setFont(FontSingleton.getInstance().bonGodic.deriveFont(Font.BOLD, 27f));
+        jlbCarIdTitle.setFont(FontSingleton.getInstance().bonGodic.deriveFont(16f));
+        jlbOwnerName.setFont(FontSingleton.getInstance().bonGodic.deriveFont(16f));
+        jbtnSearch.setFont(FontSingleton.getInstance().bonGodic.deriveFont(16f));
 
-        jlbCommonReservationTitle.setBounds(50, 0, 100, 100);
+        jlbCommonReservationTitle.setForeground(Color.WHITE);
+        jlbCarIdTitle.setForeground(Color.WHITE);
+        jlbOwnerName.setForeground(Color.WHITE);
+        jbtnSearch.setForeground(Color.WHITE);
+        jbtnSearch.setBackground(Color.decode("#065535"));
+
+        jlbCommonReservationTitle.setBounds(50, 0, 400, 100);
         jlbCarIdTitle.setBounds(50, 50, 100, 100);
-        jlbOwnerName.setBounds(230, 90, 100, 25);
-
-        jtfCarId.setBounds(110, 90, 100, 25);
-        jtfOwnerName.setBounds(275, 90, 100, 25);
-
-        jbtnSearch.setBounds(390, 90, 60, 25);
+        jtfCarId.setBounds(120, 90, 100, 25);
+        jlbOwnerName.setBounds(240, 90, 100, 25);
+        jtfOwnerName.setBounds(295, 90, 100, 25);
+        jbtnSearch.setBounds(410, 90, 70, 25);
         jsp.setBounds(50, 125, 730, 280);
 
         add(jlbCommonReservationTitle);
         add(jlbCarIdTitle);
         add(jlbOwnerName);
-
         add(jtfCarId);
         add(jtfOwnerName);
-
         add(jbtnSearch);
-
         add(commonReservationTable);
         add(jsp);
 
--- car-maintenance-system/src/kr/co/sist/admin/manage/reserved_car/ReleasedCarView.java
+++ car-maintenance-system/src/kr/co/sist/admin/manage/reserved_car/ReleasedCarView.java
@@ -1,5 +1,7 @@
 package kr.co.sist.admin.manage.reserved_car;
 
+import java.awt.Color;
+import java.awt.Font;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -7,7 +9,9 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
+import kr.co.sist.FontSingleton;
 
+@SuppressWarnings("serial")
 public class ReleasedCarView extends JFrame {
     private JLabel jlbReleasedCarTitle, jlbCarIdTitle, jlbIdTitle;
     private JTextField jtfCarId, jtfId;
@@ -15,14 +19,13 @@ public class ReleasedCarView extends JFrame {
 
     public ReleasedCarView() {
         super("차량 정비 관리 시스템");
+        this.getContentPane().setBackground(Color.decode("#002347"));
 
         jlbReleasedCarTitle = new JLabel("출고 차량 조회");
         jlbCarIdTitle = new JLabel("차량번호");
-        jlbIdTitle = new JLabel("ID");
-
         jtfCarId = new JTextField();
+        jlbIdTitle = new JLabel("ID");
         jtfId = new JTextField();
-
         jbtnSearch = new JButton("검색");
 
         String[] Header = {"차량번호", "연식", "주행거리", "정비입고일", "정비출고일", "고객명"};
@@ -30,29 +33,38 @@ public class ReleasedCarView extends JFrame {
         DefaultTableModel releasedCarModel = new DefaultTableModel(dumpData, Header);
         JTable releasedCarTable = new JTable(releasedCarModel);
         JScrollPane jsp = new JScrollPane(releasedCarTable);
-
+        releasedCarTable.getTableHeader().setFont(FontSingleton.getInstance().bonGodic.deriveFont(12f));
         releasedCarTable = new JTable(releasedCarModel);
+        releasedCarTable.setFont(FontSingleton.getInstance().bonGodic.deriveFont(11f));
+
+        jlbReleasedCarTitle.setFont(FontSingleton.getInstance().bonGodic.deriveFont(Font.BOLD, 27f));
+        jlbCarIdTitle.setFont(FontSingleton.getInstance().bonGodic.deriveFont(17f));
+        jtfCarId.setFont(FontSingleton.getInstance().bonGodic.deriveFont(17f));
+        jlbIdTitle.setFont(FontSingleton.getInstance().bonGodic.deriveFont(17f));
+        jtfId.setFont(FontSingleton.getInstance().bonGodic.deriveFont(17f));
+        jbtnSearch.setFont(FontSingleton.getInstance().bonGodic.deriveFont(14f));
+
+        jlbReleasedCarTitle.setForeground(Color.WHITE);
+        jlbCarIdTitle.setForeground(Color.WHITE);
+        jlbIdTitle.setForeground(Color.WHITE);
+        jbtnSearch.setForeground(Color.WHITE);
+        jbtnSearch.setBackground(Color.decode("#065535"));
 
         setLayout(null);
-        jlbReleasedCarTitle.setBounds(50, 0, 100, 100);
+        jlbReleasedCarTitle.setBounds(50, 0, 400, 100);
         jlbCarIdTitle.setBounds(50, 50, 100, 100);
-        jlbIdTitle.setBounds(230, 90, 100, 25);
-
-        jtfCarId.setBounds(110, 90, 100, 25);
-        jtfId.setBounds(250, 90, 100, 25);
-
+        jtfCarId.setBounds(120, 90, 100, 25);
+        jlbIdTitle.setBounds(240, 90, 100, 25);
+        jtfId.setBounds(260, 90, 100, 25);
         jbtnSearch.setBounds(380, 90, 60, 25);
         jsp.setBounds(50, 125, 730, 280);
 
         add(jlbReleasedCarTitle);
         add(jlbCarIdTitle);
         add(jlbIdTitle);
-
         add(jtfCarId);
         add(jtfId);
-
         add(jbtnSearch);
-
         add(releasedCarTable);
         add(jsp);
 
