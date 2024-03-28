3785f68358d9c0ceeb8f0fe5f3d6f71d4997aa35
jmh <tellis0207@gmail.com>
2024-03-28 16:05:49 +0900

    feat:View 생성자 오버로딩(일반,예방/리콜),event 유효성 검증 기능 추가

--- car-maintenance-system/src/kr/co/sist/user/reserve/common/CommonReservationView.java
+++ car-maintenance-system/src/kr/co/sist/user/reserve/common/CommonReservationView.java
@@ -18,29 +18,43 @@ public class CommonReservationView extends JFrame {
     private JLabel jlReservationManagement;
     private String parts;
 
-    public CommonReservationView(String label, int viewNum, String parts) {
+    public CommonReservationView(String label, int viewNum) { // 일반정비 예약 신청 화면
+        super(label);
+        // 프레임의 배경색 설정
+        this.getContentPane().setBackground(Color.decode("#002347"));
+        jlReservationManagement = new JLabel(label);
+
+        setComponent();
+
+        ReservationDialogView rdv = new ReservationDialogView(viewNum, this); // viewNum : 정비 분류
+
+        setLayout(null);
+        jlReservationManagement.setBounds(320, 50, 400, 60);
+        rdv.setBounds(100, 100, 600, 320);
+        rdv.setBorder(new TitledBorder(new LineBorder(Color.BLACK, 1)));
+
+        add(jlReservationManagement); // 라벨 배치
+        add(rdv); // rdv를 CommonReservationView에 추가
+
+        // 이벤트 추가
+        CommonReservationEvent commonReservationEvent = new CommonReservationEvent(this);
+        addWindowListener(commonReservationEvent);
+
+        setBounds(500, 300, 840, 480);
+        setResizable(false);
+        setVisible(true);
+    }// CommonReservationView
+
+    public CommonReservationView(String label, int viewNum, String parts) { // 예방,리콜 예약 신청 화면
         super(label);
         this.parts = parts;
         // 프레임의 배경색 설정
         this.getContentPane().setBackground(Color.decode("#002347"));
-
-        try {
-            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException
-                | UnsupportedLookAndFeelException e) {
-            e.printStackTrace();
-        } // end catch
-
-        SwingUtilities.updateComponentTreeUI(this);
-
         jlReservationManagement = new JLabel(label);
-        jlReservationManagement.setFont(FontSingleton.getInstance().bonGodic.deriveFont(Font.BOLD, 27f));
-        ReservationDialogView rdv = new ReservationDialogView(viewNum, this); // 1은 일반 정비 예약
 
-        Font font = FontSingleton.getInstance().bonGodic.deriveFont(16f);
-        jlReservationManagement.setFont(font);
-        jlReservationManagement.setForeground(Color.WHITE);
+        setComponent();
 
+        ReservationDialogView rdv = new ReservationDialogView(viewNum, this); // viewNum : 정비 분류
 
         setLayout(null);
         jlReservationManagement.setBounds(320, 50, 400, 60);
@@ -60,10 +74,24 @@ public class CommonReservationView extends JFrame {
     }// CommonReservationView
 
 
+    private void setComponent() {
+        try {
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+                | UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        } // end catch
+
+        SwingUtilities.updateComponentTreeUI(this);
+        jlReservationManagement.setFont(FontSingleton.getInstance().bonGodic.deriveFont(Font.BOLD, 27f));
+        jlReservationManagement.setForeground(Color.WHITE);
+
+    }// setComponent
+
+
     public String getParts() {
         return parts;
     }
-
 
     // public static void main(String[] args) {
     // new CommonReservationView("일반 정비 예약", ReservationDialogView.COMMON, "부품 이름");
